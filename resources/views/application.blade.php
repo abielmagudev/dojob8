f4931fc693cd88081ec7220bf4916e3468b1d5c4
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-13 17:32:46 -0500

    Factoriza background color para body

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,8 +7,13 @@
     <title>{{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+    <style>
+        .bg-body-custom {
+            background-color: #e7edf5;
+        }
+    </style>
 </head>
-<body class="bg-light">
+<body class="bg-body-custom">
     @include('application.navbar')
     <br>
     <div class="container">

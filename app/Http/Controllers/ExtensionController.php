f07cf4611808ee42622a3490631bd496142bb0a3
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-16 05:59:43 -0500

    Refactoriza y optimiza controlador usando el metodo __call

--- app/Http/Controllers/ExtensionController.php
+++ app/Http/Controllers/ExtensionController.php
@@ -9,43 +9,21 @@ class ExtensionController extends Controller
 {
     use ResolveFormRequestsTrait;
 
+    public function __call($method, $parameters)
+    {
+        $extension = Extension::findOrFail($parameters[0]);
+
+        $method = request()->route()->getActionMethod();
+
+        $request = $this->resolveControllerFormRequest($extension->controller, $method) ?? request();
+        
+        return app($extension->controller)->callAction($method, [$request, $extension]);
+    }
+
     public function index(Request $request)
     {
         return view('extensions.index', [
             'extensions' => Extension::all(),
         ]);
-    }
-
-    public function show(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('show', [$request, $extension]);
-    }
-
-    public function create(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('create', [$request, $extension]);
-    }
-
-    public function store(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('store', [
-            $this->resolveControllerFormRequest($extension->controller, 'store') ?? $request, 
-            $extension
-        ]);
-    }
-
-    public function edit(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('edit', [$request, $extension]);
-    }
-
-    public function update(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('update', [$request, $extension]);
-    }
-
-    public function destroy(Request $request, Extension $extension)
-    {
-        return app($extension->controller)->callAction('destroy', [$request, $extension]);
     }
 }

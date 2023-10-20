65171c45fe766a2aed99b1c1d3f65c26146493c3
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-19 20:48:32 -0500

    Elimina y refactoriza componentes header

--- resources/views/components/header-sm.blade.php
+++
@@ -1,3 +0,0 @@
-@section('header')
-<p class="fs-4 text-secondary mb-4">{{ $slot }}</p>
-@endsection
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,14 @@
 @section('header')
-<p class="fs-2 mb-4">{{ $slot }}</p>
+
+@isset($preheader)       
+<p class="{{ $attributes->get('preheader-class', 'text-secondary') }} mb-0">{{ $preheader }}</p>
+@endisset
+
+<p class="{{ $attributes->get('class', 'fs-3') }} mb-0">{{ $slot }}</p>
+
+@isset($subheader)       
+<p class="{{ $attributes->get('subheader-class', 'text-secondary') }} mb-0">{{ $subheader }}</p>
+@endisset
+
+<div class="mb-4"></div>
 @endsection

b388518ff9ab3e60486c38e7122e1a00b37f70b1
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-20 04:08:55 -0500

    Refactoriza y actualiza componente modal-trigger

--- resources/views/components/modal-trigger.blade.php
+++ resources/views/components/modal-trigger.blade.php
@@ -1,10 +1,10 @@
 @if( $attributes->has('link') )
-<a id="{{ $attributes->get('id') }}" class="{{ $attributes->get('class', 'link-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" href="#!">
+<a {{ $attributes->except(['class', 'modal-id']) }} class="{{ $attributes->get('class', 'link-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" href="#!">
     {{ $slot }}
 </a>
 
 @else
-<button id="{{ $attributes->get('id') }}" class="{{ $attributes->get('class', 'btn btn-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" type="button">
+<button {{ $attributes->except(['class', 'modal-id']) }} class="{{ $attributes->get('class', 'btn btn-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" type="button">
     {{ $slot }}
 </button>
 

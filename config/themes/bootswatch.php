706fb5a83069f697143f9ec3cbdc5dd2b79ebd69
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-19 21:52:50 -0500

    Factoriza base para utilizar temas de Bootswatch posteriormente

---
+++ config/themes/bootswatch.php
@@ -0,0 +1,37 @@
+<?php
+
+// https://bootswatch.com/
+// https://cdnjs.com/libraries/bootswatch/5.3.2
+// https://bootswatch.com/{theme}/
+
+return [
+    'default' => 'flatly',
+
+    'all' => [
+        'cerulean', 
+        'cosmo', 
+        'cyborg',
+        'darkly',
+        'flatly',
+        'journal',
+        'litera',
+        'lumen',
+        'lux',
+        'materia',
+        'minty',
+        'morph',
+        'pulse',
+        'quartz',
+        'sandstone',
+        'simplex',
+        'sketchy',
+        'slate',
+        'solar',
+        'spacelab',
+        'superhero',
+        'united',
+        'vapor',
+        'yeti',
+        'zephyr',
+    ],
+];

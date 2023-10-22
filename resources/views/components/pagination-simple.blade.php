04d4e45c7bf3ab82019743fc8aa3d24e8d872ac2
Abiel Magu <abielmagu.dev@gmail.com>
2023-10-21 23:58:54 -0500

    Factoriza componente pagination

---
+++ resources/views/components/pagination-simple.blade.php
@@ -0,0 +1,19 @@
+<div class="d-flex justify-content-end align-items-center  ">
+    <ul class="pagination shadow-sm rounded">
+        <li class="page-item {{ $collection->previousPageUrl() ? '' : 'disabled' }}">
+            <a class="page-link border-0" href="{{ $collection->previousPageUrl() ?? '#!' }}" aria-label="Previous">
+                <span>Previous</span>
+            </a>
+        </li>
+        <li class="page-item disabled ">
+            <a class="page-link border-0 text-secondary bg-white" href="#!" aria-label="Current page">
+                Page {{ request('page', '1') }}
+            </a>
+        </li>
+        <li class="page-item {{ $collection->nextPageUrl() ? '' : 'disabled' }}">
+            <a class="page-link border-0" href="{{ $collection->nextPageUrl() ?? '#!' }}" aria-label="Next">
+                <span>Next</span>
+            </a>
+        </li>
+    </ul>
+</div>

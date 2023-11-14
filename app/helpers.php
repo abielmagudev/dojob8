f16e610208427c2a1912b672a1965e9839c85d8c
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-14 06:00:24 -0600

    Factoriza custom helper para generar iniciales de palabras

--- app/helpers.php
+++ app/helpers.php
@@ -23,3 +23,17 @@ if(! function_exists('bsInputInvalid') )
         return $is_invalid ? 'is-invalid' : '';
     }
 }
+
+if(! function_exists('wordInitials') )
+{
+    function wordInitials(string $string, $to_uppercase = true)
+    {
+        preg_match_all('/(?<=\b)\w/iu', $string, $matches);
+
+        if( $to_uppercase ) {
+            return mb_strtoupper( implode($matches[0]) );
+        }
+
+        return implode($matches[0]);
+    }
+}

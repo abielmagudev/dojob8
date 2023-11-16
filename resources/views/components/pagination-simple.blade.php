708a4e0285acd0e6964bcc566520b6997373224b
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-16 02:47:58 -0600

    Actualiza component pagination-simple

--- resources/views/components/pagination-simple.blade.php
+++ resources/views/components/pagination-simple.blade.php
@@ -1,18 +1,20 @@
-<div class="d-flex justify-content-end align-items-center  ">
-    <ul class="pagination shadow-sm rounded">
+<div class="d-flex justify-content-end align-items-center">
+    <ul class="pagination shadow-sm rounded me-3">
         <li class="page-item {{ $collection->previousPageUrl() ? '' : 'disabled' }}">
             <a class="page-link border-0" href="{{ $collection->previousPageUrl() ?? '#!' }}" aria-label="Previous">
                 <span>Previous</span>
             </a>
         </li>
-        <li class="page-item disabled ">
-            <a class="page-link border-0 text-secondary bg-white" href="#!" aria-label="Current page">
-                Page {{ request('page', '1') }}
-            </a>
-        </li>
         <li class="page-item {{ $collection->nextPageUrl() ? '' : 'disabled' }}">
             <a class="page-link border-0" href="{{ $collection->nextPageUrl() ?? '#!' }}" aria-label="Next">
                 <span>Next</span>
+            </a>
+        </li>
+    </ul>
+    <ul class="pagination shadow-sm rounded">
+        <li class="page-item disabled ">
+            <a class="page-link border-0 text-secondary bg-white" href="#!" aria-label="Current page">
+                Page {{ request('page', '1') }}
             </a>
         </li>
     </ul>

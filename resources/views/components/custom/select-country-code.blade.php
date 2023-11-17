1734123eaeee8dd14bc563194706cf5907180f5f
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-16 22:21:42 -0600

    Actualiza components select-country-code y select-state-code, funciones de required y class

--- resources/views/components/custom/select-country-code.blade.php
+++ resources/views/components/custom/select-country-code.blade.php
@@ -3,10 +3,12 @@
 $country_code_to_select = $countries->get( $attributes->get('old') ) ? $attributes->get('old') : $country_code_default;
 
 ?>
-<select id="countryCodeSelect" class="form-select {{ bsInputInvalid( $errors->has('country_code') ) }}" name="country_code">
-    @foreach($countries as $code => $country)
+<select id="countryCodeSelect" class="form-select {{ bsInputInvalid( $errors->has('country_code') ) }} {{ $attributes->get('class', '') }}" name="country_code" @if( $attributes->has('required') ) required @endif>
+    @if(! $attributes->has('required') )
+    <option selected label="Any country"></option>
+    @endif
 
+    @foreach($countries as $code => $country)
     <option value="{{ $code }}" {{ isSelected( ($country_code_to_select == $code) ) }}>{{ $country->get('name') }}</option>
-    
     @endforeach
 </select>
--- resources/views/components/custom/select-state-code.blade.php
+++ resources/views/components/custom/select-state-code.blade.php
@@ -5,11 +5,12 @@ $state_code_to_select = $country_selected->get('states')->get( $attributes->get(
 
 ?>
 
-<select id="stateCodeSelect" class="form-select {{ bsInputInvalid( $errors->has('state_code') ) }}" name="state_code">
+<select id="stateCodeSelect" class="form-select {{ bsInputInvalid( $errors->has('state_code') ) }} {{ $attributes->get('class', '') }}" name="state_code" @if( $attributes->has('required') ) required @endif>
+    @if(! $attributes->has('required') )
+    <option selected label="Any state of country"></option>
+    @endif
+    
     @foreach($country_selected->get('states') as $code => $state)
-    
-    
     <option value="{{ $code }}" {{ isSelected( ($state_code_to_select == $code) ) }}>{{ $state }}</option>
-    
     @endforeach
 </select>

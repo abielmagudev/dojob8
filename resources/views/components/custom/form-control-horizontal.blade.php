2df11c9ac7ed18868c6a0feedd6590ebd0230b63
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-17 22:14:49 -0600

    Actualiza el responsive del custom component form-control-horizontal

--- resources/views/components/custom/form-control-horizontal.blade.php
+++ resources/views/components/custom/form-control-horizontal.blade.php
@@ -1,8 +1,8 @@
 <div class="row mb-3 {{ $attributes->get('class', '') }}">
-    <div class="col-md {{ $attributes->get('label-class', '') }}">
+    <div class="col-md col-md-4 col-lg-3 col-xl-2 {{ $attributes->get('label-class', '') }}">
         {!! $label !!}
     </div>
-    <div class="col-md col-md-9 col-lg-10 {{ $attributes->get('slot-class', '') }}">
+    <div class="col-md col-md-8 col-lg-9 col-xl-10 {{ $attributes->get('slot-class', '') }}">
         {!! $slot !!}
     </div>
 </div>

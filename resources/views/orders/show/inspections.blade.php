cbee4db1b72aae2955e37bcba305766676f1611f
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-22 07:28:01 -0600

    Actualiza inspections en view show de Order

--- resources/views/orders/show/inspections.blade.php
+++ resources/views/orders/show/inspections.blade.php
@@ -1,47 +1,52 @@
 <x-card title="Inspections">
     <x-slot name="options">
-        <div class="d-none d-lg-inline-block">
-            <small class="me-3">Total: <b>{{ $order->inspections->count() }}</b></small>
-            <small class="me-3">Approved: <b>{{ $order->inspections->where('is_approved', 1)->count() }}</b></small>
-            <small class="me-3">Required: <b>{{ $order->job->approved_inspections_required }}</b></small>
-        </div>
         <a href="{{ route('inspections.create', $order) }}" class="btn btn-primary">
             <b>+</b>
         </a>
     </x-slot>
 
-    <div class="d-flex justify-content-around text-center text-uppercase mb-3 d-lg-none">
-        <div class="me-3">
-            <small class="d-block text-secondary">Total</small>
-            <b>{{ $order->inspections->count() }}</b>
-        </div>
-        <div class="me-3">
-            <small class="d-block text-secondary">Approved</small>
-            <b>{{ $order->inspections->where('is_approved', 1)->count() }}</b>
-        </div>
-        <div>
-            <small class="d-block text-secondary">Required</small>
-            <b>{{ $order->job->approved_inspections_required }}</b>
+    <div class="alert alert-light text-black mb-3">
+        <div class="d-flex justify-content-center text-center text-uppercase">
+            <div>
+                <small class="d-block">Total</small>
+                <b>{{ $order->inspections->count() }}</b>
+            </div>
+            <div class="mx-3 mx-md-5">
+                <small class="d-block">Approved</small>
+                <b>{{ $order->inspections->where('is_approved', 1)->count() }}</b>
+            </div>
+            <div>
+                <small class="d-block">Required</small>
+                <b>{{ $order->job->approved_inspections_required }}</b>
+            </div>
         </div>
     </div>
 
-    <x-table class="align-middle">
+    <x-table>
+        <x-slot name="thead">
+            <tr>
+                <th>Schedule</th>
+                <th>Inspector</th>
+                <th>Observations</th>
+                <th>Status</th>
+                <th></th>
+            </tr>
+        </x-slot>
         @foreach($order->inspections->sortByDesc('scheduled_date')->load('inspector') as $inspection)
-        <tr>
-            <td class="text-center">
-                <x-custom.badge-status :color="$inspection->status_color">{{ $inspection->status_label }}</x-custom.badge-status>
+        <tr @class(['align-middle' => strlen($inspection->observations) < 100])>
+            <td class="text-nowrap pe-5" style="width:1%">{{ $inspection->scheduled_date->format('D d M, Y') }}</td>
+            <td class="text-nowrap">{{ $inspection->inspector->name }}</td>
+            <td style="min-width:240px; max-width:512px">{{ $inspection->observations }}</td>
+            <td>
+                <x-badge :color="$inspection->status_color" class="text-uppercase">{{ $inspection->status_label }}</x-badge>
             </td>
-            <td class="text-center text-nowrap">{{ $inspection->scheduled_date->format('D d M, Y') }}</td>
-            <td class="text-center">{{ $inspection->inspector->name }}</td>
-            <td class="text-center">
-                @if( $inspection->hasObservations() )
-                <span class="badge text-bg-primary" data-bs-toggle="tooltip" data-bs-html="true" data-bs-title="{{ $inspection->observations }}">
-                    Observations
-                </span>    
-                           
+            <td class="text-nowrap text-end">
+                @if( $inspection->hasNotes() )
+                <button class="btn btn-outline-primary" data-bs-toggle="tooltip" data-bs-html="true" data-bs-title="- Notes -<br>{{ $inspection->notes }}">
+                    <i class="bi bi-chat-square-text"></i>
+                </button>    
                 @endif
-            </td>
-            <td class="text-end">
+
                 <a href="{{ route('inspections.edit', [$inspection, 'back' => 'order']) }}" class="btn btn-outline-warning">
                     <i class="bi bi-pencil-fill"></i>
                 </a>

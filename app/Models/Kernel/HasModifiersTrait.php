b310b0c8d3c7aa575698c9f51df82f06171f145b
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-23 05:05:09 -0600

    Factoriza trait para modificadores, usuarios que crearon y actualizaron el modelo asignado

---
+++ app/Models/Kernel/HasModifiersTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Kernel;
+
+use App\Models\User;
+
+trait HasModifiersTrait
+{
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+}

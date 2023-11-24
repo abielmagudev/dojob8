0840514f35e098950cb2078b8fec2b33ab345d84
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-24 14:45:53 -0600

    Renombra trait para asignar valores de hook users en observers e implementa en IntermediaryObserver

--- app/Observers/IntermediaryObserver.php
+++ app/Observers/IntermediaryObserver.php
@@ -3,42 +3,43 @@
 namespace App\Observers;
 
 use App\Models\Intermediary;
-use App\Observers\Kernel\HasHookModifiers;
 use App\Observers\Kernel\HasObserverConstructor;
+use App\Observers\Kernel\HookUserSetters;
 
 class IntermediaryObserver
 {
-    use HasHookModifiers;
+    use HookUserSetters;
     use HasObserverConstructor;
 
     public function creating(Intermediary $intermediary)
     {
-        $this->storingBy($intermediary);
+        $this->storingBy($intermediary, mt_rand(1,10));
+        $this->updatingBy($intermediary, mt_rand(1,10));
     }
 
     public function created(Intermediary $intermediary)
     {
-        //
+
     }
 
     public function updating(Intermediary $intermediary)
     {
-        $this->updatingBy($intermediary);
+        $this->updatingBy($intermediary, mt_rand(1,10));
     }
 
     public function updated(Intermediary $intermediary)
     {
-        //
+
     }
 
     public function deleting(Intermediary $intermediary)
     {
-        $this->deletingBy($intermediary);
+        $this->deletingBy($intermediary, mt_rand(1,10));
     }
 
     public function deleted(Intermediary $intermediary)
     {
-        //
+
     }
 
     public function restored(Intermediary $intermediary)

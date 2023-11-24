a3d670b23eda080c8e260f1b366adb34a7f9c21f
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-24 04:43:03 -0600

    Refactoriza trait de hooks modifiers para Observers

--- app/Observers/Kernel/HasHookModifiers.php
+++ app/Observers/Kernel/HasHookModifiers.php
@@ -2,35 +2,39 @@
 
 namespace App\Observers\Kernel;
 
+use Closure;
 use Illuminate\Database\Eloquent\Model;
 
 trait HasHookModifiers
 {
+    public function withoutTimestamps(Model $model, Closure $closure)
+    {
+        $model->timestamps = false;
+
+        $closure($model);
+    }
+
     public function storingBy(Model $model)
     {
-        if( $model->exists('created_by') ) {
-            $model->created_by = mt_rand(1, 10);
-        }
+        $model->created_by = mt_rand(1, 10);
 
         $this->updatingBy($model);
     }
 
     public function updatingBy(Model $model)
     {
-        if(! $this->request->isMethod('delete') )
-        {
-            if( $model->exists('updated_by') ) {
-                $model->updated_by = mt_rand(1, 10);
-            }
+        if(! $this->request->isMethod('delete') ) {
+            $model->updated_by = mt_rand(1, 10);
         }
     }
 
     public function deletingBy(Model $model)
     {
         if( $model->exists('deleted_by') ) {
-            $model->timestamps = false;
-            $model->deleted_by = mt_rand(1, 10);
-            $model->save();
+            $this->withoutTimestamps($model, function ($model) {
+                $model->deleted_by = mt_rand(1, 10);
+                $model->save();
+            });
         }
     }
 }

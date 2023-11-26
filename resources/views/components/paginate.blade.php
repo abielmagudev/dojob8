401229d76ffbc1756127693274b3dbacc0cc1b07
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-26 04:29:27 -0600

    Refactoriza component paginate para imprimir raw las urls

--- resources/views/components/paginate.blade.php
+++ resources/views/components/paginate.blade.php
@@ -1,8 +1,8 @@
 <div class="btn-group {{ $attributes->get('class', '') }}">
-    <a href="{{ $attributes->get('previous') ?? '#!' }}" class="btn btn-primary {{ $attributes->get('previous') ?: 'disabled' }}">
+    <a href="{!! $attributes->get('previous') ?? '#!' !!}" class="btn btn-primary {{ $attributes->get('previous') ?: 'disabled' }}">
         <i class="bi bi-caret-left-fill"></i>
     </a>
-    <a href="{{ $attributes->get('next') ?? '#!' }}" class="btn btn-primary {{ $attributes->get('next') ?: 'disabled' }}">
+    <a href="{!! $attributes->get('next') ?? '#!' !!}" class="btn btn-primary {{ $attributes->get('next') ?: 'disabled' }}">
         <i class="bi bi-caret-right-fill"></i>
     </a>
 </div>

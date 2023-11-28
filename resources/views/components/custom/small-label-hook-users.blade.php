8d7ef545ff68b8bb519851fa662b07ee464052ae
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-28 05:04:37 -0600

    Factoriza componente custom para los hook-users

---
+++ resources/views/components/custom/small-label-hook-users.blade.php
@@ -0,0 +1,16 @@
+<x-small-label label="Created">
+    <span class="d-block">{{ $model->created_at }}</span>
+    <span class="d-block">{{ $model->creator->name }}</span>
+</x-small-label>
+
+<x-small-label label="Updated">
+    <span class="d-block">{{ $model->updated_at }}</span>
+    <span class="d-block">{{ $model->updater->name }}</span>
+</x-small-label>
+
+@if( $attributes->has('show-deleted') )  
+<x-small-label label="Deleted">
+    <span class="d-block">{{ $model->deleted_at ?? '' }}</span>
+    <span class="d-block">{{ $model->deleter ? $model->deleter->name : '' }}</span>
+</x-small-label>
+@endif

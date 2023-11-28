6c54e6586b4dbb8a9326ddbcfb67e2af1aa9c270
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-28 06:15:34 -0600

    Refactoriza y mejora componente error

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,4 +1,19 @@
-@error( $attributes->get('name') )
-<div class="text-danger small">{{ $message }}</div>
-{{-- <div class="invalid-feedback">{{ $message }}</div> --}}
-@enderror
+<div class="small ms-1 {{ $attributes->get('class', '') }}">
+    @error( $attributes->get('name') )
+    <span class="text-danger">{{ $message }}</span>
+    
+    @else
+        @if( $slot->isNotEmpty() )
+
+        @if( $attributes->has('important') )   
+        <b class="text-danger">*</b>
+        @endif
+
+        <span class="text-secondary">
+            {!! $slot !!}
+        </span>
+
+        @endif
+    
+    @enderror
+</div>

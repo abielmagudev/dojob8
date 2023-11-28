c9c36c80e7471e512a13118dd403be532511d548
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-28 05:23:11 -0600

    Actualiza componente de small-label-hook-users en caso de no tener un hook user

--- resources/views/components/custom/small-label-hook-users.blade.php
+++ resources/views/components/custom/small-label-hook-users.blade.php
@@ -1,11 +1,11 @@
 <x-small-label label="Created">
     <span class="d-block">{{ $model->created_at }}</span>
-    <span class="d-block">{{ $model->creator->name }}</span>
+    <span class="d-block">{{ $model->creator ? $model->creator->name : '' }}</span>
 </x-small-label>
 
 <x-small-label label="Updated">
     <span class="d-block">{{ $model->updated_at }}</span>
-    <span class="d-block">{{ $model->updater->name }}</span>
+    <span class="d-block">{{ $model->updater ? $model->updater->name : '' }}</span>
 </x-small-label>
 
 @if( $attributes->has('show-deleted') )  

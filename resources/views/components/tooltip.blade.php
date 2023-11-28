a364812f937c4cb17ce54bbb146623f201bf05fb
Abiel Magu <abielmagu.dev@gmail.com>
2023-11-28 09:16:59 -0600

    Factoriza componente tooltip

---
+++ resources/views/components/tooltip.blade.php
@@ -0,0 +1,19 @@
+<span 
+    class="{{ $attributes->get('class', 'd-inline-block') }}" 
+    data-bs-toggle="tooltip" 
+    data-bs-title="{{ $title }}"
+    
+    @if( $attributes->has('placement') )
+    data-bs-placement="{{ $attributes->get('placement') }}"
+    @endif
+
+    @if( $attributes->has('custom-class') )
+    data-bs-custom-class="{{ $attributes->get('custom-class') }}"
+    @endif
+
+    @if( $attributes->has('html') )
+    data-bs-html="true"
+    @endif
+>
+    {!! $slot !!}
+</span>

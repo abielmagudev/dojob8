a12a473f912c85032b924bd22cfdf25eff3432e4
Abiel Magu <abielmagu.dev@gmail.com>
2023-12-05 06:36:18 -0600

    Factoriza componentes links para tel y email

---
+++ resources/views/components/link-email.blade.php
@@ -0,0 +1,3 @@
+<a href="mailto:{{ $href }}" class="{{ $attributes->get('class', 'link-primary') }}">
+    {!! $slot !!}
+</a>
---
+++ resources/views/components/link-phone.blade.php
@@ -0,0 +1,3 @@
+<a href="tel:{{ $href }}" class="{{ $attributes->get('class', 'link-primary') }}">
+    {!! $slot !!}
+</a>

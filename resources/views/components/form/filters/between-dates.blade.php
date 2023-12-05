b7296080def864965e359fb23003ed0b8c7008fa
Abiel Magu <abielmagu.dev@gmail.com>
2023-12-04 20:27:34 -0600

    Actualiza component filter de between-dates

--- resources/views/components/form/filters/between-dates.blade.php
+++ resources/views/components/form/filters/between-dates.blade.php
@@ -2,11 +2,11 @@
     <div class="row g-3">
         <div class="col-sm">
             <label for="filterBetweenFromDateInput" class="form-label">From</label>
-            <input id="filterBetweenFromDateInput" class="form-control" type="date" name="between_dates[]" value="{{ $request->input('between_dates.0') }}">
+            <input id="filterBetweenFromDateInput" class="form-control" type="date" name="between_dates[from]" value="{{ $request->input('between_dates.from') }}">
         </div>
         <div class="col-sm">
             <label for="filterBetweenToDateInput" class="form-label">To</label>
-            <input id="filterBetweenToDateInput" class="form-control" type="date" name="between_dates[]" value="{{ $request->input('between_dates.1') }}">
+            <input id="filterBetweenToDateInput" class="form-control" type="date" name="between_dates[to]" value="{{ $request->input('between_dates.to') }}">
         </div>
     </div>
 </div>

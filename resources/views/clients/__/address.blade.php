4eb8b60b6a83221ab0ec02e44ae1229020e27bc4
Abiel Magu <abielmagu.dev@gmail.com>
2023-12-05 03:33:10 -0600

    Actualiza view address para implementar except al mostrar la informacion de Client

--- resources/views/clients/__/address.blade.php
+++ resources/views/clients/__/address.blade.php
@@ -1,6 +1,25 @@
+<?php if(! isset($except) ||! is_array($except) ) $except = [] ?>
 <address>
+    @if(! in_array('full_name', $except) )
     <span>{{ $client->full_name }}</span><br>
+    @endif
+
+    @if(! in_array('street', $except) )
     <span>{{ $client->street }}</span><br>
+    @endif
+
+    @if(! in_array('location', $except) )
     <span>{{ $client->city }}, {{ $client->state_code }} {{ $client->zip_code }}</span><br>
-    <span>{{ $client->country_name }}</span>
+    @endif
+
+    @if(! in_array('country', $except) )
+    <span>{{ $client->country_name }}</span><br>
+    @endif
+
+    @if(! in_array('google_maps', $except) )
+    <small>
+        <a href="{{ $client->url_search_address_google_maps }}">Google Maps</a>
+    </small>
+    <br>
+    @endif
 </address>

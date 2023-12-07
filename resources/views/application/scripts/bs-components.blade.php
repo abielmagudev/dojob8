df01629f283ca77e608c09c824e58f0346245aeb
Abiel Magu <abielmagu.dev@gmail.com>
2023-12-07 10:41:57 -0600

    actualiza js de componentes de Bs para los Toasts

--- resources/views/application/scripts/bs-components.blade.php
+++ resources/views/application/scripts/bs-components.blade.php
@@ -1,4 +1,9 @@
 <script>
+// Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Toasts
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
 </script>

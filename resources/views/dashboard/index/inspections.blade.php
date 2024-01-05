9f95eefc8180dea08e22c0797b05057612ab561c
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-05 16:35:15 -0600

    Corrige y actualiza section de inspections de Dashboard

--- resources/views/dashboard/index/inspections.blade.php
+++ resources/views/dashboard/index/inspections.blade.php
@@ -5,15 +5,19 @@
             <span class="d-block text-uppercase small">Total</span>
         </div>
         <div class="col-sm mb-3 mb-md-0">
-            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->isOnHold())->count() }}</span>
+            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->hasStatus('pending'))->count() }}</span>
             <span class="d-block text-uppercase small">Pending</span>
         </div>
         <div class="col-sm mb-3 mb-md-0">
-            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->isApproved())->count() }}</span>
-            <span class="d-block text-uppercase small">Approved</span>
+            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->hasStatus('on hold'))->count() }}</span>
+            <span class="d-block text-uppercase small">On hold</span>
         </div>
         <div class="col-sm mb-3 mb-md-0">
-            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->isFailed())->count() }}</span>
+            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->hasStatus('passed'))->count() }}</span>
+            <span class="d-block text-uppercase small">Passed</span>
+        </div>
+        <div class="col-sm mb-3 mb-md-0">
+            <span class="fs-3">{{ $inspections->filter(fn($inspection) => $inspection->hasStatus('failed'))->count() }}</span>
             <span class="d-block text-uppercase small">Failed</span>
         </div>
     </div>

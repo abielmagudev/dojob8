113bc6aca8c86fd9a6232ba8245cfec15b497e49
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-09 05:42:08 -0600

    Actualiza modal de filters de Member

--- resources/views/members/index/modal-member-filters.blade.php
+++ resources/views/members/index/modal-member-filters.blade.php
@@ -14,8 +14,8 @@
             <label for="isCrewMemberSelect" class="form-label">Crew member</label>
             <select id="isCrewMemberSelect" name="is_crew_member" class="form-select">
                 <option label="Any option"></option>
-                <option value="1" {{ isSelected( $request->get('is_crew_member') === "1" ) }}>Yes, it can be in crews</option>
-                <option value="0" {{ isSelected( $request->get('is_crew_member') === "0" ) }}>No, it cannot be in crews</option>
+                <option value="1" {{ isSelected( $request->get('is_crew_member') === "1" ) }}>Yes, can be in crews</option>
+                <option value="0" {{ isSelected( $request->get('is_crew_member') === "0" ) }}>No, cannot be in crews</option>
             </select>
         </div>
 

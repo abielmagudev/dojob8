fa10eeeb5a85a3c3da916a49faf4ccefba0a3d4f
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-12 04:39:53 -0600

    Corrige y actualiza componentes privades de Member

--- resources/views/members/__/contact-table-cell.blade.php
+++ resources/views/members/__/contact-table-cell.blade.php
@@ -1,6 +1,6 @@
-@foreach($member->contact_data_collection->filter() as $key => $value)
+@foreach($member->contact_data->filter() as $key => $value)
     <x-tooltip title="{{ ucfirst($key) }}">
-        <span class="badge text-bg-light">
+        <span class="badge border">
             <?php $prefix = $key <> 'email' ? 'tel' : 'mailto' ?>
             <a href="{{ $prefix }}:{{ $value }}" class="text-decoration-none">{{ $value }}</a>
         </span>
--- resources/views/members/__/contact.blade.php
+++ resources/views/members/__/contact.blade.php
@@ -1,6 +1,4 @@
-<?php if(! isset($except) ||! is_array($except) ) $except = [] ?>
-
-@foreach($member->contact_data_collection->filter() as $key => $value)
+@foreach($member->contact_data->filter() as $key => $value)
     <span class="badge text-bg-light text-start">
         <span class="d-inline-block " style="width:48px">{{ ucfirst($key) }}</span>
         <?php $prefix = $key <> 'email' ? 'tel' : 'mailto' ?>

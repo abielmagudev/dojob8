d39a5ddd5934e0d3052cf496938775673cfd2ecd
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-12 05:24:46 -0600

    Actualiza componente pagination

--- resources/views/components/pagination-simple-model.blade.php
+++ resources/views/components/pagination-simple-model.blade.php
@@ -1,21 +1,17 @@
 <div class="d-flex justify-content-end align-items-center">
-    <ul class="pagination shadow-sm rounded me-3">
-        <li class="page-item {{ $collection->previousPageUrl() ? '' : 'disabled' }}">
-            <a class="page-link border-0" href="{{ $collection->previousPageUrl() ?? '#!' }}" aria-label="Previous">
-                <span>Previous</span>
-            </a>
-        </li>
-        <li class="page-item {{ $collection->nextPageUrl() ? '' : 'disabled' }}">
-            <a class="page-link border-0" href="{{ $collection->nextPageUrl() ?? '#!' }}" aria-label="Next">
-                <span>Next</span>
-            </a>
-        </li>
-    </ul>
-    <ul class="pagination shadow-sm rounded">
-        <li class="page-item disabled ">
-            <a class="page-link border-0 text-bg-secondary" href="#!" aria-label="Current page">
-                Page {{ request('page', '1') }}
-            </a>
-        </li>
-    </ul>
+    <div class="me-3">
+        <ul class="pagination shadow-sm rounded m-0">
+            <li class="page-item {{ $collection->previousPageUrl() ? '' : 'disabled' }}">
+                <a class="page-link border-0" href="{{ $collection->previousPageUrl() ?? '#!' }}" aria-label="Previous">
+                    <span>Previous</span>
+                </a>
+            </li>
+            <li class="page-item {{ $collection->nextPageUrl() ? '' : 'disabled' }}">
+                <a class="page-link border-0" href="{{ $collection->nextPageUrl() ?? '#!' }}" aria-label="Next">
+                    <span>Next</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div>Page {{ request('page', '1') }}</div>
 </div>

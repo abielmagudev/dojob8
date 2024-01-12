1bf0091a4954bebe932f54233bdcde92f8700b28
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-12 15:40:08 -0600

    Actualiza componente form-field-horizontal para que label sea opcional

--- resources/views/components/form-field-horizontal.blade.php
+++ resources/views/components/form-field-horizontal.blade.php
@@ -1,6 +1,6 @@
 <div class="row mb-3 {{ $attributes->get('class', '') }}">
     <label for="{{ $attributes->get('for', '') }}" class="col-md col-md-3 col-xl-2 col-form-label {{ $attributes->get('label-class', '') }}">
-        {!! $label !!}
+        {!! $attributes->get('label', '') !!}
     </label>
     <div class="col-md col-md-9 col-xl-10 {{ $attributes->get('slot-class', '') }}">
         {!! $slot !!}

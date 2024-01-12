c85fc7145b22f92357a48dece747f17975bdb0dc
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-12 07:00:09 -0600

    Actualiza boton unfinished de WorkOrder

--- resources/views/work-orders/__/button-unfinished.blade.php
+++ resources/views/work-orders/__/button-unfinished.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ workOrderUrlGenerator('unfinished', $parameters) }}" class="btn btn-warning btn-sm">
+<a href="{{ workOrderUrlGenerator('unfinished', $parameters) }}" class="{{ isset($class) ? $class : 'btn btn-warning btn-sm' }}">
     <span class="{{ $counter < 9 ? 'mx-1' : '' }}">{{ $counter }}</span>
 </a>

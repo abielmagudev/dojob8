988dba30667bf30a10fbead3944ba599beecd26f
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-12 01:47:08 -0600

    Actualiza componente privado de contact de Client

--- resources/views/clients/__/contact-table-cell.blade.php
+++ resources/views/clients/__/contact-table-cell.blade.php
@@ -1,6 +1,6 @@
 @foreach($client->contact_data_collection->filter() as $key => $value)
     <x-tooltip title="{{ ucfirst($key) }}">
-        <span class="badge">
+        <span class="badge border">
             <?php $prefix = $key <> 'email' ? 'tel' : 'mailto' ?> 
             <a href="{{ $prefix }}:{{ $value }}" class="text-decoration-none">{!! isset($mark) ? marker($mark, $value) : $value !!}</a>
         </span>

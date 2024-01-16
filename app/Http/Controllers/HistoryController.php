28f4055c05981860a85d3b1e2f69e58cd73883ad
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-16 06:10:13 -0600

    Optmiza controller de History

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -10,15 +10,17 @@ class HistoryController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $history = History::with('user.profile')
+        ->filterByInputs( $request->all() )
+        ->orderBy('id', $request->get('sort', 'desc'))
+        ->paginate(25)
+        ->appends( $request->query() );
+        
         return view('history.index', [
             'request' => $request,
             'topics' => History::getTopics(),
             'users' => User::with('profile')->withTrashed()->get(),
-            'history' => History::with('user.profile')
-                                ->filterByInputs( $request->all() )
-                                ->orderBy('id', $request->get('sort', 'desc'))
-                                ->paginate(25)
-                                ->appends( $request->query() ),
+            'history' => $history,
         ]);
     }
 }

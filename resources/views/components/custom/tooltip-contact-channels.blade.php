6c164e9de7c1a4341c8c92f1355d203f7c7d4f11
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-17 03:10:11 -0600

    Actualiza componente custom tooltip-contact-channels

--- resources/views/components/custom/tooltip-contact-channels.blade.php
+++ resources/views/components/custom/tooltip-contact-channels.blade.php
@@ -1,7 +1,7 @@
 <div>
     @foreach($channels as $key => $value)
     <x-tooltip title="{{ ucfirst($key) }}">
-        <span class="badge border">
+        <span class="{{ $attributes->get('class', 'badge') }}">
             <?php $prefix = $key <> 'email' ? 'tel' : 'mailto' ?> 
             <a href="{{ $prefix }}:{{ $value }}" class="text-decoration-none">{!! isset($mark) ? marker($mark, $value) : $value !!}</a>
         </span>

406ccaee504754b519bfc6653ee3a3971a11835f
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-17 03:18:48 -0600

    Actualiza componente privado form-search de Client

--- resources/views/clients/__/form-search.blade.php
+++ resources/views/clients/__/form-search.blade.php
@@ -1,14 +1,14 @@
 <form action="{{ route('clients.index') }}" method="get" autocomplete="off">
     <div class="input-group">
         <input 
-            class="form-control rounded-start-pill bg-transparent text-center {{ $class ?? '' }}" 
+            class="form-control border-end-0 rounded-start-pill bg-transparent text-center {{ $class ?? '' }}" 
             style="{{ $style ?? '' }}"
             id="searchClientInput" 
             type="search" 
             name="search" 
             placeholder="Search client..."
         >
-        <button class="btn border border-start-0 rounded-end-pill">
+        <button class="btn border border-start-0 rounded-end-pill px-3">
             <i class="bi bi-search"></i>
         </button>
     </div>

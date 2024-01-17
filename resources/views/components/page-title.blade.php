30f20a8721b4fc591ea0c6c9f4ac1cdf7c27a3f5
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-17 06:02:43 -0600

    Actualiza componente page-title

--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,9 +1,9 @@
-@if($attributes->has('pretitle'))
+@if( $attributes->has('pretitle') )
 <small>{!! $pretitle !!}</small>
 @endif
 
 <h1 class="mb-1">{!! $slot !!}</h1>
 
-@if($attributes->has('subtitle'))
-<p class="text-secondary-emphasis">{!! $subtitle !!}</p>
+@if( isset($subtitle) )
+<div class="text-secondary-emphasis mb-3">{!! $subtitle !!}</div>
 @endif

3c249304813bb583aae19dc087ccc429cb8a0df8
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-18 13:48:36 -0600

    Refactoriza y actualiza views

--- resources/views/contractors/__/address.blade.php
+++ resources/views/contractors/__/address.blade.php
@@ -1,7 +1,7 @@
 <div>
     <address>
         <span>{{ $contractor->street }}</span><br>
-        <span>{{ $contractor->address_data->only(['city_name','state_name','country_name'])->implode(', ') }}</span><br>
+        <span>{{ $contractor->address_data->only(['city_name','state_name','country_code'])->implode(', ') }}</span><br>
         <span>ZIP {{ $contractor->zip_code }}</span><br>
     </address>
 </div>

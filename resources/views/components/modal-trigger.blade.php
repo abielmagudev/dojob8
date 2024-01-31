bb59bad91f04870336afcece89a569c7a76eb4d1
Abiel Magu <abielmagu.dev@gmail.com>
2024-01-31 01:21:26 -0600

    Actualiza orden de atributos del componente modal-trigger

--- resources/views/components/modal-trigger.blade.php
+++ resources/views/components/modal-trigger.blade.php
@@ -1,10 +1,10 @@
 @if( $attributes->has('link') )
-<a {{ $attributes->except(['class', 'modal-id', 'link']) }} class="{{ $attributes->get('class', 'link-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" href="#!">
+<a {{ $attributes->except(['class', 'modal-id', 'link']) }} class="{{ $attributes->get('class', 'link-primary') }}" data-bs-target="#{{ $attributes->get('modal-id') }}" data-bs-toggle="modal" href="#!">
     {{ $slot }}
 </a>
 
 @else
-<button {{ $attributes->except(['class', 'modal-id']) }} class="{{ $attributes->get('class', 'btn btn-primary') }}" data-bs-toggle="modal" data-bs-target="#{{ $attributes->get('modal-id') }}" type="button">
+<button {{ $attributes->except(['class', 'modal-id']) }} class="{{ $attributes->get('class', 'btn btn-primary') }}" data-bs-target="#{{ $attributes->get('modal-id') }}" data-bs-toggle="modal" type="button">
     {{ $slot }}
 </button>
 

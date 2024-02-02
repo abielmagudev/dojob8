4b8eba5b53fd003eb9c6d8db4be1282d8276928b
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-02 06:52:43 -0600

    Refactoriza para corregir e implementar nuevas funcionalidades en el componente card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,32 +1,31 @@
 <div class="card shadow-sm border-0 {{ $attributes->get('class', '') }}" style="{{ $attributes->get('style', '') }}">
 
-    @if( isset($title) || isset($custom_title) isset($options) || isset($dropoptions) )
+    @if( isset($title) || isset($custom_title) || isset($options) || isset($dropoptions) )
     <div class="card-header border-bottom-0 py-3">
         <div class="d-flex align-items-center justify-content-between">
 
             {{-- Left --}}
             <div>
-                @isset( $custom_title )
+                @isset($custom_title)
                 {!! $custom_title !!}
 
                 @else
-                <b>{{ $title ?? '' }}</b>
+                <b class="d-block">{{ $title ?? '' }}</b>
 
                 @endisset
-                <br>
                 <small>{{ $attributes->get('subtitle', '') }}</small>
             </div>
 
             {{-- Right --}}
-            <div>
+            <div class="d-flex align-items-center justify-content-between">
                 @isset($options)       
-                <div class="{{ $attributes->get('options-class', 'd-inline-block align-middle') }}">
+                <div class="{{ $attributes->get('options-class', '') }}">
                     {!! $options !!}
                 </div>
                 @endisset
 
                 @isset($dropoptions)              
-                <div class="d-inline-block align-middle">
+                <div class="{{ $attributes->get('dropoptions-class', '') }}">
                     <div class="dropdown dropdown-menu-end">
                         <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-three-dots-vertical"></i>

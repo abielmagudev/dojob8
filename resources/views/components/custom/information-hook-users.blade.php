d36b8004fec97cd2b5bfb5cda514638855a184df
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-02 08:05:43 -0600

    Refactoriza componente custom hook users

--- resources/views/components/custom/information-hook-users.blade.php
+++ resources/views/components/custom/information-hook-users.blade.php
@@ -1,14 +1,18 @@
+@if( $model->creator )
 <p>
     <small>Created</small><br>
-    <span>{{ $model->created_at }}</span><br>
+    <span>{{ $model->created_at ?? '' }}</span><br>
     <span>{{ $model->creator ? $model->creator->name : '' }}</span>
-</p>
+</p> 
+@endif
 
+@if( $model->updater )  
 <p>
     <small>Updated</small><br>
-    <span>{{ $model->updated_at }}</span><br>
+    <span>{{ $model->updated_at ?? '' }}</span><br>
     <span>{{ $model->updater ? $model->updater->name : '' }}</span>
 </p>
+@endif
 
 @if( $model->deleter )  
 <p>

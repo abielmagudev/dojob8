197fa9ebb8f349828b52efad61878ad5c8f80812
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-07 04:49:39 -0600

    Factoriza nuevos traits separando los status de available y active

---
+++ app/Models/Kernel/Traits/HasActiveStatus.php
@@ -0,0 +1,75 @@
+<?php 
+
+namespace App\Models\Kernel\Traits;
+
+trait HasActiveStatus
+{
+    // Attributes
+
+    public function getActiveStatusAttribute()
+    {
+        return $this->isActive() ? 'active' : 'inactive';
+    }
+
+    public function getActiveValueAttribute()
+    {
+        return $this->is_active;
+    }
+
+
+    // Validators
+
+    public function isActive()
+    {
+        return (bool) $this->is_active;
+    }
+
+    public function isInactive()
+    {
+        return ! $this->isActive();
+    }
+
+
+    // Scopes
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', 1);
+    }
+
+    public function scopeInactive($query)
+    {
+        return $query->where('is_active', 0);
+    }
+
+    public function scopeUpdateActive($query)
+    {
+        return $query->update(['is_active' => 1]);
+    }
+
+    public function scopeUpdateInactive($query)
+    {
+        return $query->update(['is_active' => 0]);
+    }
+
+
+    // Filters
+
+    public function scopeFilterByActive($query, $value)
+    {
+        return in_array($value, [0, 1]) ? $query->where('is_active', $value) : $query;
+    }
+
+
+    // Actions
+
+    public function saveActive()
+    {
+        return $this->fill(['is_active' => 1])->save();
+    }
+
+    public function saveInactive()
+    {
+        return $this->fill(['is_active' => 0])->save();
+    }
+}
---
+++ app/Models/Kernel/Traits/HasAvailableStatus.php
@@ -0,0 +1,75 @@
+<?php 
+
+namespace App\Models\Kernel\Traits;
+
+trait HasAvailableStatus
+{
+    // Atributes
+
+    public function getAvailableStatusAttribute()
+    {
+        return $this->isAvailable() ? 'available' : 'unavailable';
+    }
+
+    public function getAvailableValueAttribute()
+    {
+        return $this->is_available;
+    }
+
+
+    // Validators
+
+    public function isAvailable()
+    {
+        return (bool) $this->is_available;
+    }
+
+    public function isUnavailable()
+    {
+        return ! $this->isAvailable();
+    }
+
+
+    // Scopes
+
+    public function scopeAvailable($query)
+    {
+        return $query->where('is_available', 1);
+    }
+
+    public function scopeUnavailable($query)
+    {
+        return $query->where('is_available', 0);
+    }
+
+    public function scopeUpdateAvailable($query)
+    {
+        return $query->update(['is_available' => 1]);
+    }
+
+    public function scopeUpdateUnavailable($query)
+    {
+        return $query->update(['is_available' => 0]);
+    }
+
+
+    // Filters
+
+    public function scopeFilterByAvailable($query, $value)
+    {
+        return in_array($value, [0, 1]) ? $query->where('is_available', $value) : $query;
+    }
+
+
+    // Actions
+
+    public function saveAvailable()
+    {
+        return $this->fill(['is_available' => 1])->save();
+    }
+
+    public function saveUnavailable()
+    {
+        return $this->fill(['is_available' => 0])->save();
+    }
+}

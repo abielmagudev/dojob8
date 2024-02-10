3fad4c9d93ecee7f6fb41a7d7905f364426963c9
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-10 08:11:01 -0600

    Actualiza y refactoriza componente card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,41 +1,41 @@
 <div class="card shadow-sm border-0 {{ $attributes->get('class', '') }}" style="{{ $attributes->get('style', '') }}">
 
-    @if( isset($title) || isset($custom_title) || isset($options) || isset($dropoptions) )
+    @if( isset($title) || isset($options) || isset($dropoptions) )
     <div class="card-header border-bottom-0 py-3">
-        <div class="d-flex align-items-center justify-content-between">
+        <div class="row align-items-center">
 
             {{-- Left --}}
-            <div>
-                @isset($custom_title)
-                {!! $custom_title !!}
-
-                @else
-                <b class="d-block">{{ $title ?? '' }}</b>
-
-                @endisset
-                <small>{{ $attributes->get('subtitle', '') }}</small>
+            <div class="col-sm mb-3 mb-md-0">
+                <div class="{{ $attributes->get('title-class', 'fw-bold') }}">
+                    {!! $title !!}
+                </div>
+                <small class="{{ $attributes->get('subtitle-class', '') }}">{{ $attributes->get('subtitle', '') }}</small>
             </div>
 
             {{-- Right --}}
-            <div class="d-flex align-items-center justify-content-between">
-                @isset($options)       
-                <div class="{{ $attributes->get('options-class', '') }}">
-                    {!! $options !!}
-                </div>
-                @endisset
+            <div class="col-sm">
+                <div class="d-flex align-items-center justify-content-between justify-content-md-end">
 
-                @isset($dropoptions)              
-                <div class="{{ $attributes->get('dropoptions-class', '') }} ms-3">
-                    <div class="dropdown dropdown-menu-end">
-                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-three-dots-vertical"></i>
-                        </button>
-                        <ul class="dropdown-menu border-0 shadow">
-                          {!! $dropoptions !!}
-                        </ul>
-                      </div>
+                    @isset($options)       
+                    <div class="{{ $attributes->get('options-class', '') }}">
+                        {!! $options !!}
+                    </div>
+                    @endisset
+    
+                    @isset($dropoptions)              
+                    <div class="{{ $attributes->get('dropoptions-class', '') }} ms-3">
+                        <div class="dropdown dropdown-menu-end">
+                            <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-three-dots-vertical"></i>
+                            </button>
+                            <ul class="dropdown-menu border-0 shadow">
+                              {!! $dropoptions !!}
+                            </ul>
+                          </div>
+                    </div>
+                    @endisset
+
                 </div>
-                @endisset
             </div>
         </div>
     </div>
@@ -50,9 +50,7 @@
     @endif
 
     @isset($footer)
-    <div class="card-footer">
-        {!! $footer !!}
-    </div>
+    <div class="card-footer">{!! $footer !!}</div>
     @endisset
-    
+
 </div>

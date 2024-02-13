698050b9c7da60870e1be055d0c5deb010d6c81b
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-13 12:18:01 -0600

    Refactoriza para corregir y optimizar y renombra componente script de Checker

---
+++ resources/views/components/scripts/Checkerky.blade.php
@@ -0,0 +1,41 @@
+<script>
+class Checker
+{
+    toggle = false;
+
+    checkbox_id_begin = null;
+
+    id_trigger = null;
+
+    constructor($checkbox_id_begin, $id_trigger = 'checkerButton')
+    {
+        this.checkbox_id_begin = $checkbox_id_begin;
+        this.id_trigger = $id_trigger;
+    }
+
+    trigger() {
+        return document.getElementById(this.id_trigger);
+    }
+
+    checkboxes() {
+        return document.body.querySelectorAll(`input[type="checkbox"][id^="${this.checkbox_id_begin}"]`)
+    }
+
+    listen()
+    {
+        let self = this;
+
+        if( this.trigger() )
+        {
+            this.trigger().addEventListener('click', function (evt)
+            {
+                evt.preventDefault()
+    
+                self.toggle = !self.toggle;
+                self.trigger().classList.toggle('active', self.toggle)
+                self.checkboxes().forEach(item => item.checked = self.toggle)
+            })
+        }
+    }
+}
+</script>

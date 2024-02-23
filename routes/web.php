a8a990d6ebb61294c07c753d568836be50358440
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-23 15:31:50 -0600

    Elimina comentario en routes

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', fn() => redirect()->route('login') );
 Route::fallback(fn() => redirect()->route('login'));
 
 Route::middleware(['auth'])->group(function () {

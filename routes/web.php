f03e88800b39958f1b8397ea5b54fd776e7556ff
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-23 13:56:36 -0600

    Factoriza logica de redireccionamiento cuando la ruta esta vacia

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', fn() => redirect()->route('login') );
+
 Route::middleware(['auth'])->group(function () {
     // Route::get('/', fn() => redirect()->route('orders.index') );
     

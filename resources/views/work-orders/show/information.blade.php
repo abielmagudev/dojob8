f4cc79c8091e32da4194d4efe49a94d6d4246e96
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-24 04:17:33 -0600

    Refactoriza orden de informacion de WorkOrder

--- resources/views/work-orders/show/information.blade.php
+++ resources/views/work-orders/show/information.blade.php
@@ -6,8 +6,7 @@
     @endslot
 
     @include('work-orders.show.information.job')
+    @includeWhen($work_order->job->hasExtensions(), 'work-orders.show.information.extensions')
     @include('work-orders.show.information.timeline')
     @include('work-orders.show.information.participants')
-    @includeWhen($work_order->job->hasExtensions(), 'work-orders.show.information.extensions')
-    
 </x-card>

5b49468025e51a354afc007d428eb8e370a699d1
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-26 07:51:18 -0600

    Factoriza logica base para work orders e inspections del usuario con role Worker

--- app/Http/Controllers/DashboardController/WorkerDashboard.php
+++ app/Http/Controllers/DashboardController/WorkerDashboard.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers\DashboardController;
 
+use App\Models\Inspection;
+use App\Models\WorkOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class WorkerDashboard
 {
     public static function response(Request $request)
     {
-        return view('dashboard.worker.index', compact('request'));
+        $today = Carbon::today();
+        $worker = auth()->user()->profile;
+        $work_orders_id = $worker->has('work_orders')->get() ? $worker->work_orders->pluck('id')->toArray() : false;
+        $inspections_id = $worker->has('inspections')->get() ? $worker->inspections->pluck('id')->toArray() : false;
+        $work_orders = ! empty($work_orders_id) ? WorkOrder::whereIn('id', $work_orders_id)->whereDate('scheduled_date', $today)->get() : collect([]);
+        $inspections = ! empty($inspections_id) ? Inspection::whereIn('id', $inspections_id)->whereDate('scheduled_date', $today)->get() : collect([]);
+
+        return view('dashboard.worker.index', [
+            'request' => $request,
+            'today' => $today,
+            'inspections' => $inspections,
+            'work_orders' => $work_orders,
+        ]);
     }
 }
--- resources/views/dashboard/worker/index.blade.php
+++ resources/views/dashboard/worker/index.blade.php
@@ -1,4 +1,45 @@
 @extends('application')
 @section('content')
-<h1>Dashboard Worker</h1>
+
+@section('header')
+<x-page-title>Dashboard (Worker)</x-page-title>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-sm">
+        <div class="border rounded p-3 text-center">
+
+            <p class="text-uppercase">Work Orders</p>
+            @if( $work_orders->count() )
+            <a href="<?= route('work-orders.index', ['scheduled' => $today]) ?>" class="display-1 text-white text-decoration-none">
+                {{ $work_orders->count() }}
+            </a>
+            
+            @else
+            <span class="display-1">0</span>
+            
+            @endif
+
+        </div>
+    </div>
+    <div class="col-sm">
+        <div class="border rounded p-3 text-center">
+
+            <p class="text-uppercase">Inspections</p>
+            @if( $inspections->count() )
+            <a href="<?= route('inspections.index', ['scheduled' => $today]) ?>" class="display-1 text-white text-decoration-none">
+                {{ $inspections->count() }}
+            </a>
+            
+            @else
+            <span class="display-1">0</span>
+
+            @endif
+
+        </div>
+    </div>
+</div>
+@endsection
+
 @endsection

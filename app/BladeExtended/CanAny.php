cbe15d62e0b167ff51522a2d411c3f86dc694056
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-25 22:13:34 -0600

    Factoriza blade directive para verificar cualquier persmiso que posea el usuario autenticado

---
+++ app/BladeExtended/CanAny.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\BladeExtended;
+
+use Illuminate\Support\Facades\Blade;
+
+class CanAny
+{
+    public static function boot()
+    {
+        Blade::directive('canAny', function ($permissions) {
+            return "<?php if( auth()->check() && auth()->user()->canAny({$permissions}) || empty({$permissions}) ): ?>";
+        });
+
+        Blade::directive('endcanAny', function () {
+            return '<?php endif; ?>';
+        });
+    }
+}

6d0702dc4dc6ca59ab2466ed4b5d4ec5895b0d91
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-26 16:02:16 -0600

    Actualiza dashboard de worker

--- resources/views/dashboard/worker/index.blade.php
+++ resources/views/dashboard/worker/index.blade.php
@@ -7,10 +7,11 @@
 
 @section('content')
 <div class="row">
+    <!-- Work Orders -->
     <div class="col-sm mb-3">
-        <div class="border rounded p-3 text-center">
-
+        <x-box class="text-center">
             <p class="text-uppercase">Work Orders</p>
+
             @if( $work_orders->count() )
             <a href="<?= route('work-orders.index', ['scheduled' => $today]) ?>" class="display-1 text-white text-decoration-none">
                 {{ $work_orders->count() }}
@@ -21,12 +22,14 @@
             
             @endif
 
-        </div>
+        </x-box>
     </div>
-    <div class="col-sm mb-3">
-        <div class="border rounded p-3 text-center">
 
+    <!-- Inspections -->
+    <div class="col-sm mb-3">
+        <x-box  class="text-center">
             <p class="text-uppercase">Inspections</p>
+
             @if( $inspections->count() )
             <a href="<?= route('inspections.index', ['scheduled' => $today]) ?>" class="display-1 text-white text-decoration-none">
                 {{ $inspections->count() }}
@@ -36,6 +39,8 @@
             <span class="display-1">0</span>
 
             @endif
+
+        </x-box>
 
         </div>
     </div>

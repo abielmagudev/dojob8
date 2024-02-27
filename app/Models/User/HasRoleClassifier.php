bf80523a7533e8ae8baa2413d4bb91b9c749eee8
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-27 07:38:33 -0600

    Renombra y refactoriza clasificador de roles de usuario e implementa actualizaciones, factoriza trait para User

---
+++ app/Models/User/HasRoleClassifier.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\User;
+
+trait HasRoleClassifier
+{
+    // Validators
+
+    public function hasAdminRole(): bool
+    {
+        return UserRoleClassifier::collectionAdminRoles()->contains($this->role_name);
+    }
+
+    public function hasFieldRole(): bool
+    {
+        return UserRoleClassifier::collectionFieldRoles()->contains($this->role_name);
+    }
+
+    public function hasPartnerRole(): bool
+    {
+        return UserRoleClassifier::collectionPartnerRoles()->contains($this->role_name);
+    }
+}
--- database/seeders/Development/UserSeeder.php
+++ database/seeders/Development/UserSeeder.php
@@ -6,7 +6,7 @@ use App\Models\Agency;
 use App\Models\Contractor;
 use App\Models\Member;
 use App\Models\User;
-use App\Models\User\UserRole;
+use App\Models\User\UserRoleClassifier;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -18,18 +18,9 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'test',
-            'email' => 'test@mail.com',
-            'password' => 'password',
-            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'profile_type' => Member::class,
-            'profile_id' => 1,
-            'last_session_at' => now(),
-        ])->assignRole('SuperAdmin');
+        $this->createUserSuperAdminRole();
 
-
-        $member_roles = UserRole::getRolesByClassname(Member::class)->reject(fn($value) => $value == 'worker');
+        $member_roles = UserRoleClassifier::getRolesBelongModel(Member::class);
 
         User::factory(30)->create()->each(function ($user) use ($member_roles) {
             if( $user->profile_type == Contractor::class ) {
@@ -42,9 +33,22 @@ class UserSeeder extends Seeder
 
             if( $user->profile_type == Member::class )
             {
-                $role = !$user->profile->isCrewMember() ? $member_roles->random() : 'worker';
+                $role = ! $user->profile->isCrewMember() ? $member_roles->except('worker')->random() : 'worker';
                 $user->assignRole($role);
             }
         });
+    }
+
+    public function createUserSuperAdminRole()
+    {
+        User::create([
+            'name' => 'test',
+            'email' => 'test@mail.com',
+            'password' => 'password',
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'profile_type' => Member::class,
+            'profile_id' => 1,
+            'last_session_at' => now(),
+        ])->assignRole('SuperAdmin');
     }
 }

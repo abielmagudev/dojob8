1c644cbb7a8563314c0d3ccd5fa925b9de79c2ba
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-27 22:53:27 -0600

    Implementa interface de profile para el usuario con role Agency

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Models\Inspection\Associated\HasInspectionsTrait;
+use App\Models\Kernel\Interfaces\Profilable;
 use App\Models\Kernel\Traits\HasActiveStatus;
 use App\Models\Kernel\Traits\HasHookUsers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Agency extends Model
+class Agency extends Model implements Profilable
 {
     use HasActiveStatus;
     use HasFactory;
@@ -25,6 +26,14 @@ class Agency extends Model
         'updated_by',
         'deleted_by',
     ];
+
+
+    // Interface
+
+    public function getProfiledNameAttribute(): string
+    {
+        return $this->name;
+    }
 
     // Actions
 

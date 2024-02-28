85f894ec566d1ce5ab53e27728d6accc7b8fcc72
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-28 00:47:17 -0600

    Renombra, refactoriza y factoriza nuevos scopes de WorkOrder

--- app/Models/WorkOrder/Traits/Scopes.php
+++ app/Models/WorkOrder/Traits/Scopes.php
@@ -9,12 +9,45 @@ trait Scopes
         return $query->where($column, 'like', "%{$value}%")->orderBy('id','asc');
     }
 
-    public function scopeIncomplete($query)
+    public function scopeIncomplete($query, $attach_pending = true)
     {
-        return $query->whereIn('status', self::getIncompleteStatuses()->toArray());
+        $statuses = self::getIncompleteStatuses();
+         
+        if(! $attach_pending ) {
+            $statuses = $statuses->reject(fn($status) => $status == 'pending');
+        }
+
+        return $query->whereIn('status', $statuses->toArray());
     }
 
-    public function scopeWithRelationshipsForIndex($query)
+    public function scopeHasMembers($query, array $members_id)
+    {
+        return $query->whereHas('members', function($query) use ($members_id){
+            $query->whereIn('member_id', $members_id);
+        });
+    }
+
+    public function scopeHasMember($query, $member_id)
+    {
+        return $query->whereHas('members', function($query) use ($member_id){
+            $query->where('member_id', $member_id);
+        });
+    }
+
+    public function scopeWithAllRelationships($query)
+    {
+        return $query->with([
+            'client',
+            'contractor',
+            'crew',
+            'job',
+            'members',
+            'reworks',
+            'warranties',
+        ]);
+    }
+
+    public function scopeWithEssentialRelationships($query)
     {
         return $query->with([
             'client',

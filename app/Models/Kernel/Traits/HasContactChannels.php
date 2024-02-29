1f53b1b072d960829096ca984ab6277911f53e86
Abiel Magu <abielmagu.dev@gmail.com>
2024-02-29 04:35:08 -0600

    Reordena collection en trait de contact channels

--- app/Models/Kernel/Traits/HasContactChannels.php
+++ app/Models/Kernel/Traits/HasContactChannels.php
@@ -12,8 +12,8 @@ trait HasContactChannels
     public function getContactDataAttribute()
     {
         return collect([
-            'phone' => $this->phone_number,
             'mobile' => $this->mobile_number,
+            'phone' => $this->phone_number,
             'email' => $this->email,
         ]);
     }

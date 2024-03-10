8733426907ab12c3602f1d393361f7bad72de7fb
Abiel Magu <abielmagu.dev@gmail.com>
2024-03-10 13:12:31 -0500

    refactoriza para corregir factory Inspection

--- database/factories/InspectionFactory.php
+++ database/factories/InspectionFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Inspection;
+use App\Models\Inspection\Kernel\InspectionStatusCatalog;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class InspectionFactory extends Factory

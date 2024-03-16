3a52735c0dbf5d08f56e60db68b384f3336dab0b
Abiel Magu <abielmagu.dev@gmail.com>
2024-03-16 07:20:43 -0500

    Crea y actualiza components

--- resources/views/components/custom/select-pending.blade.php
+++ resources/views/components/custom/select-pending.blade.php
@@ -1,9 +1,9 @@
 {{-- Pending attributes --}}
 <div class="mb-3">
-    <label for="filterPendingSelect" class="form-label">Information</label>
+    <label for="filterPendingSelect" class="form-label">Pending</label>
     <select id="filterPendingSelect" class="form-select" name="pending">
         <option label="Any" {{ isSelected( !request()->filled('pending') ) }}></option>
-        <option value="0" {{ isSelected( request()->get('pending') == '0' ) }}>No pending</option>
-        <option value="1" {{ isSelected( request()->get('pending') == '1') }}>Pending</option>
+        <option value="0" {{ isSelected( request()->get('pending') == '0' ) }}>No, without pending</option>
+        <option value="1" {{ isSelected( request()->get('pending') == '1') }}>Yes, with pending</option>
     </select>
 </div>
---
+++ resources/views/components/form/box-action-buttons.blade.php
@@ -0,0 +1,3 @@
+<div class="d-flex justify-content-end align-items-center gap-2">
+    {!! $slot !!}
+</div>
